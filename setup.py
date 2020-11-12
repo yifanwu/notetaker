3b58cae48959e5054bff70645bc79def426440c0
dorx <doris.s.xin@gmail.com>
2020-11-12 09:46:29 -0800

    datahub debug

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from glob import glob
 
 setuptools.setup(
     name='notetaker',
-    version='0.0.2',
+    version='0.0.5',
     url="https://github.com/yifanwu/notetaker",
     author="Yifan Wu",
     description="Jupyter Notebook extension to record history",
     data_files=[
-        ('share/jupyter/nbextensions/notetaker', glob('*.js'))
+        ('share/jupyter/nbextensions/notetaker', glob('notetaker/static/*')),
+        ("etc/jupyter/nbconfig/notebook.d", ["notetaker/etc/nbextension.json"]),
     ],
     packages=setuptools.find_packages(),
-    package_data={'notetaker': ['static/*']},
+    package_data={'notetaker': ['notetaker/static/*']},
+    include_package_data=True,
 )
